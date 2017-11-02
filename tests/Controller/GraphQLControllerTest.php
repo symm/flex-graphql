e8fa18ffc070c3eb393f7537f8fc4a84940ff727
Gareth Jones <me@gazj.co.uk>
2017-11-02 19:33:56 +0000

    Add broken mutation test case

--- tests/Controller/GraphQLControllerTest.php
+++ tests/Controller/GraphQLControllerTest.php
@@ -9,36 +9,14 @@ class GraphQLControllerTest extends WebTestCase
 {
     public function testEmptyQuery()
     {
-        $response = $this->doGraphQLQuery('');
+        $response = $this->performRequest('');
 
+        $this->assertEquals(200, $response->getStatusCode());
         $this->assertJsonStringEqualsJsonString(
             '{"errors":[{"message":"GraphQL Request must include at least one of those two parameters: \"query\" or \"queryId\"","category":"request"}]}',
             $response->getContent()
         );
     }
-
-    /**
-     * @dataProvider greetingProvider
-     */
-    public function testEchoQuery(string $greeting)
-    {
-        $response = $this->doGraphQLQuery(
-            '
-            query ($message: String) {
-              echo(message: $message)
-            }
-            ',
-            [
-                'message' => $greeting,
-            ]
-        );
-
-        $this->assertJsonStringEqualsJsonString(
-            '{"data":{"echo":"' . $greeting . '"}}',
-            $response->getContent()
-        );
-    }
-
 
     public function greetingProvider(): array
     {
@@ -51,11 +29,78 @@ class GraphQLControllerTest extends WebTestCase
 
     }
 
-    private function doGraphQLQuery(?string $query = null, array $variables = []): Response
+    /**
+     * @dataProvider greetingProvider
+     */
+    public function testEchoQuery(string $greeting)
+    {
+        $response = $this->doQuery(
+            '
+            query ($message: String) {
+              echo(message: $message)
+            }
+            ',
+            [
+                'message' => $greeting,
+            ]
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJsonStringEqualsJsonString(
+            '{"data":{"echo":"' . $greeting . '"}}',
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @dataProvider sumProvider
+     */
+    public function testSumMutation(int $x, int $y, int $expected)
+    {
+        $response = $this->doMutation(
+            '
+            mutation ($x: Int, $y: Int) {
+              sum(x: $x, y: $y)
+            }
+            ',
+            [
+                'x' => $x,
+                'y' => $y,
+            ]
+        );
+
+        $this->assertJsonStringEqualsJsonString(
+            '{"data":{"sum": '. $expected .' }}',
+            $response->getContent()
+        );
+    }
+
+
+    public function sumProvider(): array
+    {
+        return [
+            [1, 1, 2]
+        ];
+    }
+
+    private function doQuery(?string $query, array $variables = []): Response
+    {
+        return $this->performRequest($query, $variables, 'GET');
+    }
+
+    private function doMutation(?string $query, array $variables = []): Response
+    {
+        return $this->performRequest($query, $variables, 'POST');
+    }
+
+    private function performRequest(?string $query, array $variables = [], $method = 'GET'): Response
     {
         $client = static::createClient();
 
         $payload = [];
+
+        $queryString = '';
+        $content = '';
 
         if ($query) {
             $payload['query'] = $query;
@@ -65,13 +110,21 @@ class GraphQLControllerTest extends WebTestCase
             $payload['variables'] = $variables;
         }
 
+        if ($method === 'GET') {
+            $queryString = '?' . http_build_query($payload);
+        }
+
+        if ($method === 'POST') {
+            $content = json_encode($payload);
+        }
+
         $client->request(
-            'GET',
-            '/' . '?' . http_build_query($payload),
+            $method,
+            '/' . $queryString,
             [],
             [],
             [],
-            []
+            $content
         );
 
         $response = $client->getResponse();
