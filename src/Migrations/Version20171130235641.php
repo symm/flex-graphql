39eaf1cfeaebd5696d3cad17b48fb7b6bbb565aa
Gareth Jones <me@gazj.co.uk>
2017-12-01 00:01:58 +0000

    Add migrations

---
+++ src/Migrations/Version20171130235641.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171130235641 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE article (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , author_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , title VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66BF396750 ON article (id)');
+        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)');
+        $this->addSql('CREATE TABLE author (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BDAFD8C8BF396750 ON author (id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE article');
+        $this->addSql('DROP TABLE author');
+    }
+}
